841f8456984cdc27bbff4dd9de8efe0b2b28fbba
DuvanCamiloArenasRodriguez <dccar21@gmail.com>
2024-02-04 21:30:05 -0500

    docs: :memo: Se añade README
    Se añade README asi como el video del escritorio
    
    @CampusLands

---
+++ js/main.js
@@ -0,0 +1,6 @@
+let menu = document.querySelector(".menu")
+
+document.querySelector(".btnMenu").addEventListener("click", function(){
+    menu.classList.toggle("active")
+    // console.log("jhjdfh")
+})
